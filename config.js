07c0e600e596117a7c6107275b264f2b200f9ba2
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-09-24 09:47:29 +0700

    Oke

--- config.js
+++ config.js
@@ -16,17 +16,17 @@ global.xteam = 'ebb6251cc00f9c63'
 global.lolhuman = 'yourkey'
 
 // Other
-global.owner = ['628889616144', '628889616144', '62895334951166', '0']
+global.owner = ['628889616144', '628889616144', '0', '0', '0']
 global.wa = ['628889616144'] //Biar tampil nomor wa lu di menu
 global.premium = ['628889616144']
 global.ownername = 'ExzGanzz'
 global.botname = 'ExzBot•MD'
-global.packname = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n By © ExzGanzz'
+global.packname = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\n\n\n\n\n\n By © ExzGanzz'
 global.gc = 'https://github.com/Rmdhn-20'
 global.linkig = 'https://www.instagram.com/ekuzikaa_18'
 global.linkgc = 'Gada Cok!'
 global.limitawal = '10'
-global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n ExzBot • Multi-Device'
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\n\n\n\n\n\n\n ExzBot • Multi-Device'
 global.sessionName = 'sesi'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⌕'
